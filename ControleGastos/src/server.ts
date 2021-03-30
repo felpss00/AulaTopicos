fa01c4af7ada1250b82c0fa916e0124c51528a0c
felpss00 <fellipe_krasniakp@hotmail.com>
2021-03-30 19:39:08 -0300

    atualizacoes

--- ControleGastos/src/server.ts
+++ ControleGastos/src/server.ts
@@ -9,7 +9,18 @@ console.clear();
 
 app.get("/", (request: Request , response: Response) =>
 {
-    response.send("Hello World!");
+    response.send("Hello World com ts-node-dev teste!");
+});
+
+app.get("/jogo/listar", (request: Request , response: Response)=> {
+    const jogo =   
+    {
+        titulo: "CS",
+        plataforma: "PC",
+        valor: 50.99,
+        genero: "FPS"
+    };
+    response.json(jogo);
 });
 
 
